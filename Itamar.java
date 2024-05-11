b5490e39e63a652e15cc19c508213c700030e5dd
itamarten <itamaru1@gmail.com>
2024-05-11 13:53:04 +0300

    Itamar Devops

--- Itamar.java
+++ Itamar.java
@@ -1,7 +1,7 @@
 public class Itamar{
 	public static void main(String[] args){
 		for (int i=1;i<=10; i++){
-			System.out.println("itamar"+i);
+			System.out.println("itamar Devops"+i);
 		}
 	}
 }
